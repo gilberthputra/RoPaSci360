0b1c93e02eb0d34075d0e07f97030d4e15321edb
gilberthputra <80492691+gilberthputra@users.noreply.github.com>
2021-04-23 17:27:35 +1000

    Update state.py

--- Part B/ADDITIONAL_PYLON/state.py
+++ Part B/ADDITIONAL_PYLON/state.py
@@ -6,15 +6,18 @@ import itertools
 import collections
 
 HEX_RANGE = range(-4, +4+1)
+# Create the board and all legal coordinates for RoPaSci360
 ALL_HEXES = frozenset(
         Hex(r, q) for r in HEX_RANGE for q in HEX_RANGE if -r-q in HEX_RANGE
     )
-HEX_STEPS = [Hex(r, q) for r, q in [(1,-1),(1,0),(0,1),(-1,1),(-1,0),(0,-1)]]
-
+HEX_STEPS = [Hex(r, q) for r, q in [(1,-1),(1,0),(0,1),(-1,1),(-1,0),(0,-1)]] # Neighbours of the current pieces.
 
 BEATS_WHAT = {'r': 's', 'p': 'r', 's': 'p'}
 WHAT_BEATS = {'r': 'p', 'p': 's', 's': 'r'}
 
 class RoPaSci360:
     def __init__(self)
-        upper_tokens: tuple
+        upper_tokens: tuple # Set the upper tokens to be a type of immutable set of tuple
+        lower_tokens: tuple # ------- lower ---------------------------------------------
+
+    
